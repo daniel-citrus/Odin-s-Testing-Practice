3bc6f65c9f4e5b88d35f8eea94c3cd53f1ec04fd
Daniel <dandcalvo@gmail.com>
2024-10-28 16:57:57 -0700

    start caesarCipher

---
+++ src/caesarCipher.js
@@ -0,0 +1,9 @@
+describe('caesar cipher encryption', () => {
+    describe('ciphers strings');
+    it('(regular)', () => {});
+    it('with intergers', () => {});
+    it('with integers and symbols ', () => {});
+    it('without any letters', () => {});
+    it('with a negative shift factor', () => {});
+    it('', () => {});
+});
