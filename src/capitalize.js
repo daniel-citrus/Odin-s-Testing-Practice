837223b46bb90dfb1d248e08873abb49e241e394
Daniel <dandcalvo@gmail.com>
2024-10-28 14:35:06 -0700

    capitalize test passes

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,3 +1,24 @@
 export function capitalize(input) {
-    return input.toUpperCase();
+    if (input === null || input === undefined) {
+        return '';
+    }
+
+    input = new String(input).toString();
+
+    // find the first letter
+    // slice index 0 to first letter index
+    // capitalize first letter
+    // slice together the 2 strings
+
+    for (let i = 0; i < input.length; i++) {
+        if (/^[a-zA-Z]$/.test(input[i])) {
+            return (
+                input.slice(0, i) +
+                input.charAt(i).toUpperCase() +
+                input.slice(i + 1)
+            );
+        }
+    }
+
+    return input;
 }
--- src/tests.test.js
+++ src/tests.test.js
@@ -4,11 +4,13 @@ describe('capitalize first letter of an input', () => {
     it('capitalizes the first letter of input', () => {
         expect(capitalize('daniel')).toBe('Daniel');
         expect(capitalize('citrus')).toBe('Citrus');
+        expect(capitalize('CITRUS')).toBe('CITRUS');
     });
     it('capitalizes the first letter of a string that contains numbers', () => {
         expect(capitalize('4citrus4')).toBe('4Citrus4');
+        expect(capitalize('4CITRUS4')).toBe('4CITRUS4');
         expect(capitalize('444citrus4')).toBe('444Citrus4');
-        expect(capitalize('444c4444')).toBe('444C444');
+        expect(capitalize('44c444c4')).toBe('44C444c4');
     });
     it('accepts integers and returns strings', () => {
         expect(capitalize(1234)).toBe('1234');
