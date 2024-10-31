fe3a6017f63b6c89acb9cc9c1186c6b4654d53a1
Daniel <dandcalvo@gmail.com>
2024-10-30 17:00:14 -0700

    last problem

---
+++ src/analyzeArray.test.js
@@ -0,0 +1,48 @@
+const testData = [
+    {
+        array: [1, 8, 3, 4, 2, 6],
+        result: {
+            average: 4,
+            min: 1,
+            max: 8,
+            length: 6,
+        },
+    },
+    {
+        array: [1, -5, -45, 5],
+        result: {
+            average: 4,
+            min: 1,
+            max: 8,
+            length: 6,
+        },
+    },
+    {
+        array: [4],
+        result: {
+            average: 4,
+            min: 4,
+            max: 4,
+            length: 1,
+        },
+    },
+    {
+        array: [],
+        result: {
+            average: null,
+            min: null,
+            max: null,
+            length: 0,
+        },
+    },
+];
+
+describe('analyze an array', () => {
+    describe('returns an object containing', () => {
+        it('average', () => {});
+        it('min', () => {});
+        it('max', () => {});
+        it('length', () => {});
+    });
+});
+
