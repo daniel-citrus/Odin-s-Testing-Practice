a2291b499984e9a55fce4581d397460cff7e842c
Daniel <dandcalvo@gmail.com>
2024-11-06 16:59:16 -0800

    clean array testing

---
+++ src/analyzeArray.js
@@ -0,0 +1,71 @@
+export function analyze(array) {
+    array = cleanArray(array);
+
+    return {
+        average: getAverage(array),
+        min: getMin(array),
+        max: getMax(array),
+        length: array.length,
+    };
+}
+
+function cleanArray(array) {
+    array = array.map((element) => {
+        return +element;
+    });
+
+    array = array.filter((element) => {
+        
+        if (element === null || element === undefined || element === NaN) {
+            return false;
+        }
+
+        return true;
+    });
+}
+
+function getAverage(array) {
+    if (!array.length) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    for (let num of array) {
+        sum += num;
+    }
+
+    return sum / array.length;
+}
+
+function getMin(array) {
+    if (!array.length) {
+        return null;
+    }
+
+    let min = Number.POSITIVE_INFINITY;
+
+    for (let num of array) {
+        if (min > num) {
+            min = num;
+        }
+    }
+
+    return min;
+}
+
+function getMax(array) {
+    if (!array.length) {
+        return null;
+    }
+
+    let max = Number.NEGATIVE_INFINITY;
+
+    for (let num of array) {
+        if (max < num) {
+            max = num;
+        }
+    }
+
+    return max;
+}
--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -1,6 +1,9 @@
+import { analyze } from './analyzeArray';
+
 const testData = [
     {
         array: [1, 8, 3, 4, 2, 6],
+        description: 'integers',
         result: {
             average: 4,
             min: 1,
@@ -10,15 +13,17 @@ const testData = [
     },
     {
         array: [1, -5, -45, 5],
+        description: 'negative integers',
         result: {
-            average: 4,
-            min: 1,
-            max: 8,
-            length: 6,
+            average: -11,
+            min: -45,
+            max: 5,
+            length: 4,
         },
     },
     {
         array: [4],
+        description: 'a single integer',
         result: {
             average: 4,
             min: 4,
@@ -28,21 +33,42 @@ const testData = [
     },
     {
         array: [],
+        description: 'nothing',
         result: {
-            average: null,
+            average: 0,
             min: null,
             max: null,
             length: 0,
         },
     },
+    {
+        array: [null, 4, null, 5, null, null, 3],
+        description: 'integers and null values',
+        result: {
+            average: 4,
+            min: 3,
+            max: 5,
+            length: 3,
+        },
+    },
+    {
+        array: [null, '4', null, '5', null, null, 3],
+        description: 'integers, string integers, and null values',
+        result: {
+            average: 4,
+            min: 3,
+            max: 5,
+            length: 3,
+        },
+    },
 ];
 
 describe('analyze an array', () => {
-    describe('returns an object containing', () => {
-        it('average', () => {});
-        it('min', () => {});
-        it('max', () => {});
-        it('length', () => {});
+    describe('that contains', () => {
+        for (let data of testData) {
+            it(data.description, () => {
+                expect(analyze(data.array)).toMatchObject(data.result);
+            });
+        }
     });
 });
-
